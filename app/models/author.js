f1cc3feb36882dc89dd91d02a78dd3a2ec7adf41
DNoblega <diego.noblega@medismart.live>
2025-05-05 10:18:49 -0400

    minorfix

--- app/models/author.js
+++ app/models/author.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const AuthorModel = mongoose.model('autors', { 
+const authorScheme = new mongoose.Schema({ 
     nombre: { type: String, required: true },
     bio: { type: String, required: false },
     fechaNacimiento: { type: Date, required: true },
     nacionalidad: { type: String, required: true },
     Libros: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
-});
+})
+const AuthorModel = mongoose.model('autors', authorScheme);
 
 module.exports = {AuthorModel};
--- app/models/book.js
+++ app/models/book.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const BookModel = mongoose.model('libros', { 
+const bookScheme = new mongoose.Schema({ 
     titulo: { type: String, required: true },
     resumen: { type: String, required: false },
     genero: { type: String, required: true },
     publicacion: { type: Date, required: true },
     disponible: { type: Boolean, required: true }
 });
+const BookModel = mongoose.model('libros', bookScheme);
 
 module.exports = { BookModel };
